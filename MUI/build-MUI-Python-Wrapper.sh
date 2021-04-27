01cd117ea65679e5478cdae29bf76c0cc5005133
Wendi Liu <wendi.liu1988@icloud.com>
2021-04-27 12:20:34 +0100

    add mui

---
+++ MUI/build-MUI-Python-Wrapper.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Exit on error
+set -e
+
+#salloc --nodes=1 --tasks-per-node=1 --cpus-per-task=1 --time=01:0:00 --partition=standard --qos=standard --account=c01-eng
+
+source /work/c01/c01/wendiliu/app/FEniCS/V2019.1.0/fenics2019_eCSE_FSI.conf
+
+module list
+
+export INSTALL_DIR=/work/c01/c01/wendiliu/app/MUI
+
+cp source/v1.0_dev.tar.gz ./
+tar -xzf v1.0_dev.tar.gz
+rm v1.0_dev.tar.gz
+
+patch_file=$INSTALL_DIR/patched-MUI-Python-Makefile-FSI 
+cp "${patch_file}" $INSTALL_DIR/v1.0_dev/wrappers/Python/Makefile
+
+cd $INSTALL_DIR/v1.0_dev/wrappers/Python
+srun --distribution=block:block --hint=nomultithread make COMPILER=GCC package
+#make COMPILER=GCC mui4py_mod
+#make COMPILER=GCC package
+exit
+
+source /work/c01/c01/wendiliu/app/FEniCS/V2019.1.0/fenics2019_eCSE_FSI.conf
+module list
+export INSTALL_DIR=/work/c01/c01/wendiliu/app/MUI
+cd $INSTALL_DIR/v1.0_dev/wrappers/Python
+make pip-install
