bf9e871fc575eb34bdf44e717d2200e03fa4a272
Charles Moulinec <charles.moulinec@stfc.ac.uk>
2021-12-08 16:14:07 +0000

    Update InstallHPC.sh - Minor changes

--- Code_Saturne/V6.0.6/InstallHPC.sh
+++ Code_Saturne/V6.0.6/InstallHPC.sh
@@ -1,31 +1,26 @@
 #!/bin/sh
 
-INSTALL_FOLDER=/work/c01/c01/wendiliu/app_test
-cd ${INSTALL_FOLDER}/code_saturne/V6.0.6
-
 #################################
 ## Which version of the code ? ##
 #################################
 
-CODE_VERSION=6.0.6
-KER_VERSION=${CODE_VERSION}
-KERNAME=code_saturne-${KER_VERSION}
+export CODE_VERSION=6.0.6
+export KER_VERSION=${CODE_VERSION}
+export KERNAME=code_saturne-${KER_VERSION}
 
 ################################################
 ## Installation PATH in the current directory ##
 ################################################
 
-INSTALLPATH=`pwd`
-
+export INSTALLPATH=`pwd`
 echo $INSTALLPATH
 
 #####################################
 ## Environment variables and PATHS ##
 #####################################
   
-NOM_ARCH=`uname -s`
-
-CS_HOME=${INSTALLPATH}/${KERNAME}
+export NOM_ARCH=`uname -s`
+export CS_HOME=${INSTALLPATH}/${KERNAME}
 
 export PATH=$CS_HOME/bin:$PATH
 
@@ -40,50 +35,39 @@ rm -rf $INSTALLPATH/$KERNAME.build
 ## Kernel Installation ##
 #########################
 
-KERSRC=$INSTALLPATH/$KERNAME
-KERBUILD=$INSTALLPATH/$KERNAME.build/arch/$NOM_ARCH
-KEROPT=$INSTALLPATH/$KERNAME/arch/$NOM_ARCH
+export KERSRC=$INSTALLPATH/$KERNAME
+export KERBUILD=$INSTALLPATH/$KERNAME.build/arch/$NOM_ARCH
+export KEROPT=$INSTALLPATH/$KERNAME/arch/$NOM_ARCH
 
-export KEROPT
-
-PARMETISPATH=${INSTALL_FOLDER}/FEniCS/V2019.1.0/boost/parmetis-4.0.3
-PTSCOTCHPATH=${INSTALL_FOLDER}/FEniCS/V2019.1.0/boost/scotch_6.1.0
+export PTSCOTCHPATH=${INSTALL_FOLDER}/FEniCS/V2019.1.0/boost/scotch_6.1.0
 
 mkdir -p $KERBUILD
 cd $KERBUILD
 
-
-$KERSRC/configure          \
---enable-shared            \
---disable-nls              \
---without-modules          \
---disable-gui              \
---enable-long-gnum         \
---disable-mei              \
---enable-debug             \
---with-metis-include=$PARMETISPATH/include   \
---with-metis-lib=$PARMETISPATH/lib           \
---with-scotch-include=$PTSCOTCHPATH/include  \
---with-scotch-lib=$PTSCOTCHPATH/lib          \
---prefix=$KEROPT           \
---with-blas=$CRAY_LIBSCI_PREFIX_DIR          \
+$KERSRC/configure                                            \
+--enable-shared                                              \
+--disable-nls                                                \
+--without-modules                                            \
+--disable-gui                                                \
+--enable-long-gnum                                           \
+--disable-mei                                                \
+--enable-debug                                               \
+--with-scotch-include=$PTSCOTCHPATH/include                  \
+--with-scotch-lib=$PTSCOTCHPATH/lib                          \
+--prefix=$KEROPT                                             \
+--with-blas=$CRAY_LIBSCI_PREFIX_DIR                          \
 --with-blas-include=$CRAY_LIBSCI_PREFIX_DIR/include          \
---with-blas-lib=$CRAY_LIBSCI_PREFIX_DIR/lib          \
---with-blas-libs=-lsci_gnu          \
-CC="cc"                    \
-CFLAGS="-O3"               \
-FC="ftn"                   \
-FCFLAGS="-O3"              \
---with-mpi             \
---host=x86_64-unknown-linux-gnu             \
-CXX="CC"                   \
+--with-blas-lib=$CRAY_LIBSCI_PREFIX_DIR/lib                  \
+--with-blas-libs=-lsci_gnu                                   \
+CC="cc"                                                      \
+CFLAGS="-O3"                                                 \
+FC="ftn"                                                     \
+FCFLAGS="-O3"                                                \
+CXX="CC"                                                     \
 CXXFLAGS="-O3"
 
-###make -j 8
-###make install
-###make distclean
+make -j 8
+make install
 
 cd $INSTALLPATH
-###rm -rf $INSTALLPATH/Kernel/ncs-${KER_VERSION}.build
-export PATH=${INSTALL_FOLDER}/code_saturne/V6.0.6/code_saturne-6.0.6/arch/Linux/bin:$PATH
 
